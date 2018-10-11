2f8f22a393fe5bb4eb87f11453b153f5c3e7f439
dmitriychebakov <whynotmonkeys2191@gmail.com>
2018-10-11 16:04:29 +0300

    3 commit

---
+++ src/app/main-page/services-tutors/services-tutors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services-tutors',
+  templateUrl: './services-tutors.component.html',
+  styleUrls: ['./services-tutors.component.css']
+})
+export class ServicesTutorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
